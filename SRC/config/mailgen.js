f4e4a26aa304177f5621af424b9f367c493a1821
Ephraim7223 <basseyephraim55@gmail.com>
2025-08-26 00:06:09 +0100

    added cloudinary link

--- SRC/config/mailgen.js
+++ SRC/config/mailgen.js
@@ -5,7 +5,7 @@ export const mailGenerator = new Mailgen ({
     product: {
         name: 'TUTOR',
         link: 'https://dummy-link.com', 
-        logo: '',
+        logo: 'https://res.cloudinary.com/dvikxcdh3/image/upload/v1755863637/WhatsApp_Image_2025-08-22_at_12.53.31_s6at1p.jpg',
         logoHeight: '100px'
     }
 })
